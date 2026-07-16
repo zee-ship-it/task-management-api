4a9107c9edb685f334e11fd16c437c5a101c7e97
zee-ship-it <zeeeeee2005@gmail.com>
2026-07-16 18:57:59 +0500

    Stage 1: root nad health endpoints

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 app = FastAPI()
+ #def hello_server():
+#   return  "Hello, server!"
+
+#1. GET "/"
 @app.get("/")
-def hello_server():
-  return  "Hello, server!"
+def get_root():
+  return {
+    "name": "Task API",
+    "version": "1.0",
+    "endpoints": ["/tasks"]
+  }
+#2. GET "/health"
+@app.get("/health")
+def get_health():
+  return {"status": "ok"}
+
