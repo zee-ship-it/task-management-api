5178a80192fe5931fdf6b15cd8aaa028ee22dbed
zee-ship-it <zeeeeee2005@gmail.com>
2026-07-23 23:57:42 +0500

    unnecessary class removed

--- app/repository.py
+++ app/repository.py
@@ -60,60 +60,7 @@ class InmemoryTaskRepository(TaskRepository):
                 return True
         return False
 
-# class PostgresTaskRepository(TaskRepository):
-#     def get_all_tasks(self):
-#         connection = get_db_connection()
-#         cursor = connection.cursor()
-#         cursor.execute("SELECT id, title, done FROM tasks ORDER BY id ASC;")
-#         tasks = cursor.fetchall()
-#         cursor.close()
-#         connection.close()
-#         return tasks
 
-#     def get_by_id(self, task_id: int):
-#         connection = get_db_connection()
-#         cursor = connection.cursor()
-#         cursor.execute("SELECT id, title, done FROM tasks WHERE id = %s;", (task_id,))
-#         task = cursor.fetchone()
-#         cursor.close()
-#         connection.close()
-#         return task
-
-#     def create_task(self, title: str):
-#         connection = get_db_connection()
-#         cursor = connection.cursor()
-#         cursor.execute(
-#             "INSERT INTO tasks (title, done) VALUES (%s, %s) RETURNING id, title, done;",
-#             (title, False),
-#         )
-#         task = cursor.fetchone()
-#         connection.commit()
-#         cursor.close()
-#         connection.close()
-#         return task
-
-#     def update_task(self, task_id: int, title: str, done: bool):
-#         connection = get_db_connection()
-#         cursor = connection.cursor()
-#         cursor.execute(
-#             "UPDATE tasks SET title = %s, done = %s WHERE id = %s RETURNING id, title, done;",
-#             (title, done, task_id),
-#         )
-#         updated_task = cursor.fetchone()
-#         connection.commit()
-#         cursor.close()
-#         connection.close()
-#         return updated_task
-
-#     def delete_task(self, task_id: int):
-#         connection = get_db_connection()
-#         cursor = connection.cursor()
-#         cursor.execute("DELETE FROM tasks WHERE id = %s RETURNING id;", (task_id,))
-#         deleted_row = cursor.fetchone()
-#         connection.commit()
-#         cursor.close()
-#         connection.close()
-#         return deleted_row is not None
 
 class SqliteTaskRepository(TaskRepository):
     def __init__(self, db_path="tasks.db"):
